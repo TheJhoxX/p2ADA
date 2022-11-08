34b21e6020019e21973324395cd975a810f7c0a4
TheJhoxX <victorjorgesibaja@gmail.com>
2022-11-08 19:37:52 +0100

    LECTURA LINEAS FICHERO

--- src/main.java
+++ src/main.java
@@ -3,40 +3,44 @@ Víctor Jorge Sibaja
 Pablo Gutiérrez Martínez
  */
 import java.io.*;
+import java.util.Arrays;
 
 public class main {
 
-    public static BufferedReader getEntrada(){
+    public static FileReader getEntrada(){
         File fichero = null;
         FileReader fr = null;
-        BufferedReader br = null;
 
         try {
-            fichero = new File("./entrada.txt");
+            fichero = new File("entrada.txt");
             fr = new FileReader(fichero);
-            br = new BufferedReader(fr);
+
+
         }
         catch (Exception e){
             e.printStackTrace();
         }
-        finally {
-            try{
-                if (null != fr) {
-                    fr.close();
-                }
-            }
-            catch (Exception e2){
-                e2.printStackTrace();
-            }
 
-        }
-        return br;
+        return fr;
     }
 
     public static void main(String args[]) throws IOException {
-        BufferedReader br = getEntrada();
-        System.out.println(br.readLine());
-        System.out.println(br.readLine());
+        FileReader fr = getEntrada();
+        BufferedReader br = new BufferedReader(fr);
+
+        //Numero de piezas de cada torre para el caso i
+        String[] n = (br.readLine()).split(" ");
+
+        System.out.println(Arrays.toString(n));
+        /*Cerrar el fichero*/
+        try{
+            if (null != fr) {
+                fr.close();
+            }
+        }
+            catch (Exception e2){
+            e2.printStackTrace();
+        }
 
     }
 }
